8f9431dcc6c10cdb00c9273c86cf7ea499c2cb0b
maazin-4546 <maazin.noorisys@gmail.com>
2025-06-17 10:57:51 +0800

    modifications

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get('/about', (req, res) => {
   res.send('About Page')
 })
 
+app.get('/service', (req, res) => {
+  res.send('service Page')
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
